f6e1722fbd51d3e341dc0144121bfe6701a0401f
Dylan Nichols <nichodo@g.toiohomai.ac.nz>
2017-08-14 11:38:12 +1200

    changed bad input checking

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
+// Explain program to user
 var txt
-alert("This program converts from Celsius to Fahrenheit and vice versa. You will be asked to input a unit and then a temperature")
+alert("This program converts from Celsius to Fahrenheit and vice versa. You will be asked to input a unit and then a temperature. Click OK to begin")
 
+// Get input from user for unit, check for bad input
+do {
 var unit = prompt("Please enter C to convert to Celsius or F for Fahrenheit","Type here")
 if (unit != "C" && unit != "F") {
-    txt = "You messed up the input try again"
+    alert("Invalid input! Click OK to try again")
 }
+} while (unit != "C" && unit != "F")
 
+// Get input from user for temperature, check for bad input
 var temp = 0.0
-temp = prompt("Please enter a temperature","Type here")
-
-var output = 0.0
-if (isNaN(temp)) {
-    txt = "You messed up the input try again"
-} else {
-    if (unit == "C") {
-        output = (temp - 32) * 5 / 9
-        txt = "Your temperature in Celsisus comes to " + output
-    } else {
-        output = temp * 9 / 5 + 32
-        txt = "Your temperature in Fahrenheit comes to " + output
+do {
+    temp = prompt("Please enter a temperature","Type here")
+    if (isNaN(temp)) {
+        alert("You must input a number. Click OK to try again")
     }
+} while (isNaN(temp))
+
+// Output to user
+var output = 0.0
+
+if (unit == "C") {
+    output = (temp - 32) * 5 / 9
+    txt = "Your temperature in Celsisus comes to " + output
+} else {
+    output = temp * 9 / 5 + 32
+    txt = "Your temperature in Fahrenheit comes to " + output
 }
 
 alert(txt)
